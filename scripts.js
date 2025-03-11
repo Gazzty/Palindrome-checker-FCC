0b97e020515bcea65b430f3ec4176e7153fca778
Gazzty <tomasgustavo99@gmail.com>
2025-03-11 15:50:25 -0300

    First commit, page should work fine

---
+++ scripts.js
@@ -0,0 +1,35 @@
+function isPalindrome(word){
+    let reversedWord = word.split("").reverse().join('')
+
+    console.log(reversedWord)
+
+    if (word === reversedWord){
+        return true
+    }
+    return false
+}
+
+// Check Palindrome when the user click the button
+const submit = document.getElementById('check-btn')
+const input = document.getElementById('text-input')
+const answer = document.getElementById('result')
+
+// Save original answer color 
+const originalColor = answer.style.color
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault() // To avoid page reloading
+    const userInput = input.value
+
+    answer.style.color = originalColor
+
+    if(userInput === ""){
+        alert("Please input a value")
+    }
+    else if(isPalindrome(userInput)){
+        answer.innerText = `${userInput} is a palindrome.`
+    }
+    else{
+        answer.innerText = `${userInput} is not a palindrome.`
+    }
+})
